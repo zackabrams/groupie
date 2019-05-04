a3d738ee0770a0a522c5d7e14238084634686af9
Zack Abrams <zackabrams@dyn-160-39-139-1.dyn.columbia.edu>
2019-05-04 18:14:21 -0400

    hopeful

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+var SlackBot = require('slackbots');
+
+// create a bot
+const envKey = process.env.GROUPIE_BOT_TOKEN
+var bot = new SlackBot({
+  token: GROUPIE_BOT_TOKEN,
+  name: 'groupie'
+})
+
+
+bot.on('start', function(data) {
+    // more information about additional params https://api.slack.com/methods/chat.postMessage
+    var params = {
+        icon_emoji: ':robot_face:'
+    };
+
+    // define channel, where bot exist. You can adjust it there https://my.slack.com/services
+    bot.postMessageToChannel('shhhhtesting', "hello! I'm starting", params);
+
+    channellist = bot.getChannels();
+
+});
+
+bot.on('message', function(data) {
+    var params = {
+        icon_emoji: ':robot_face:'
+    };
+
+    if (data.type === 'message' && data.text.includes('!board')) {
+        userlist = bot.getUsers();
+        userarray = userlist._value.members;
+        obj1 = userarray.find(o => o.id === data.user);
+        username = obj1.profile.first_name;
+
+        chanellist = bot.getChannels();
+        channelarray = channellist._value.channels;
+        obj2 = channelarray.find(o => o.id === data.channel);
+        console.log(obj2);
+        channelname = obj2.name;
+
+        messagetext = "Beep Boop! Hey @ board! "+username+" summoned you, saying:"+data.text.replace("!board","");
+        bot.postMessageToChannel(channelname, messagetext, params);
+        return;
+    };
+
+    if (data.type === 'message' && data.text.includes('!dailies')) {
+        userlist = bot.getUsers();
+        userarray = userlist._value.members;
+        obj1 = userarray.find(o => o.id === data.user);
+        username = obj1.profile.first_name;
+
+        chanellist = bot.getChannels();
+        channelarray = channellist._value.channels;
+        obj2 = channelarray.find(o => o.id === data.channel);
+        console.log(obj2);
+        channelname = obj2.name;
+
+        messagetext = "Beep Boop! Hey @ dailies! "+username+" summoned you, saying:"+data.text.replace("!dailies","");
+        bot.postMessageToChannel(channelname, messagetext, params);
+        return;
+    };
+
+    if (data.type === 'message' && data.text.includes('!social')) {
+        userlist = bot.getUsers();
+        userarray = userlist._value.members;
+        obj1 = userarray.find(o => o.id === data.user);
+        username = obj1.profile.first_name;
+
+        chanellist = bot.getChannels();
+        channelarray = channellist._value.channels;
+        obj2 = channelarray.find(o => o.id === data.channel);
+        console.log(obj2);
+        channelname = obj2.name;
+
+        messagetext = "Beep Boop! Hey @ social media team! "+username+" summoned you, saying:"+data.text.replace("!social","");
+        bot.postMessageToChannel(channelname, messagetext, params);
+        return;
+    };
+});
