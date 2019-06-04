1c942231cd614c051247743725348a6ff786fea4
Zack Abrams <zackabrams@Zacks-MacBook-Pro.local>
2019-06-03 20:20:28 -0400

    API Fix

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var SlackBot = require('slackbots');
 var fs = require('fs');
 
 // create a bot
+const token1 = process.env.GROUPIE_BOT_TOKEN
+print(token1)
 var bot = new SlackBot({
-  token: process.env.GROUPIE_BOT_TOKEN,
+  token: token1,
   name: 'groupie'
 })
 
