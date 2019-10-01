f443c9caa5424d9b8e00dbf296850a935a1eba66
Zack Abrams <zack.abrams@columbia.edu>
2019-10-01 11:55:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ bot.on('message', function(data) {
         console.log(obj2);
         channelname = obj2.name;
 
-        messagetext = "Beep Boop! Hey social team, <@UD0KZERSA>, <@UD02MNUDR>, <@UCZ5HD85A>, and <@U7J4GSQTB>! "+username+" summoned you, saying:"+data.text.replace("@social","");
+        messagetext = "Beep Boop! Hey social team, <@UD02MNUDR> and <@U7J4GSQTB>! "+username+" summoned you, saying:"+data.text.replace("@social","");
         bot.postMessageToChannel(channelname, messagetext, params);
         return;
     };
